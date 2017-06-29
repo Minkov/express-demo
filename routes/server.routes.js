d82226d22e50f6db609d002c71cf18eb4fd801c9
Doncho Minkov <DonchoMinkov@gmail.com>
2017-06-29 14:46:51 +0300

    Added views and middlewares

--- routes/server.routes.js
+++ routes/server.routes.js
@@ -1,5 +1,13 @@
 const { Router } = require('express');
 
+const items = [{
+    id: 1,
+    name: 'Cuki',
+}, {
+    id: 2,
+    name: 'John',
+}];
+
 const attach = (app) => {
     const router = new Router();
 
@@ -15,13 +23,22 @@ const attach = (app) => {
                 interests: ['Math', 'JS'],
             });
         }).get('/all', (req, res) => {
-            res.send(`<h1>All</h1>
-    <ul>
-        <li><a href="#item-1">Go to Item 1</a></li>
-        <li><a href="#item-2">Go to Item 2</a></li>
-        <li><a href="#item-3">Go to Item 3</a></li>
-    </ul>
-        `);
+            res.render('all', {
+                model: items,
+            });
+        })
+        .get('/:id', (req, res, next) => {
+            console.log(req.user);
+            const id = parseInt(req.params.id, 10);
+            const item = items.find((i) => i.id === id);
+
+            if (!item) {
+                return res.redirect('/404');
+            }
+
+            return res.render('details', {
+                model: item,
+            });
         });
 
     app.use('/', router);
--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-const express = require('express');
+/* globals __dirname */
 
-const app = express();
+const path = require('path');
+
+const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, done) => {
+    req.user = {
+        username: 'Gosho',
+    };
+
+    done();
+});
+
+app.use('/libs',
+    express.static(
+        path.join(__dirname, './node_modules'))
+);
+
+app.use('/static',
+    express.static(
+        path.join(__dirname, './static'))
+);
+
+app.get('/404', (req, res) => {
+    return res.send('<h1>Error</h1>');
+});
+
 require('./routes/server.routes')(app);
 require('./routes/api.routes').attach(app);
+
+app.get('*', (req, res) => {
+    res.redirect('/404');
+});
 
 app.listen(3001, () => console.log('Magic happens at :3001'));
